f00b2df3dda969d5d2787702bd2edf009d11528d
Kristianped <55670926+Kristianped@users.noreply.github.com>
2021-02-03 13:06:29 +0100

    Added RouterModule and routing

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,7 +9,8 @@ import {RecipeService} from '../recipe.service';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  constructor(private recipeService: RecipeService) {
+  constructor(private recipeService: RecipeService,
+              private router: Router, private route: ActivatedRoute) {
   }
 
   recipes: Recipe[] = [];
@@ -17,4 +19,7 @@ export class RecipeListComponent implements OnInit {
     this.recipes = this.recipeService.getRecipes();
   }
 
+  onNewRecipe(): void {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
 }
